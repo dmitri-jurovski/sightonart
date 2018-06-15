867541f2bdad12dc150fa54de4ed5dc6a4620e4c
DJ <dmitri.jurovski@gmail.com>
2018-06-16 00:38:50 +0300

    Fetch api from instagram and convert to data array object

--- src/app.js
+++ src/app.js
@@ -1,95 +1,25 @@
-const media = [
-    {
-      title: 'Hop on Pop',
-      description: "A delightful children's book.",
-      type: 'book',
-      contributor: 'Dr. Suess',
-      showDetail: false,
-    },
-    {
+'use strikc';
 
-
-
-      title: 'The Joy of Painting',
-      description: "Create a world of happy little trees!",
-      type: 'DVD',
-      contributor: 'Bob Ross',
-      showDetail: false
-    },
-    {
-      title: 'Supernatural: The Complete 12th Season',
-      description: "A (literally) neverending roadtrip.",
-      type: 'DVD',
-      contributor: "",
-      showDetail: false
-    },
-    {
-      title: 'Planet Earth II',
-      description: "Hours of beautiful but heart attack-inducing nature footage",
-      type: 'streaming video',
-      contributor: 'David Attenborough',
-      showDetail: false,
-    },
-    {
-      title: 'Titanic',
-      description: "The boat sinks.",
-      type: 'DVD',
-      contributor: 'James Cameron',
-      showDetail: false,
-    },
-    {
-      title: 'The Sirens of Titan',
-      description: "Mankind flung its advance agents ever outward, ever outward... it flung them like stones.",
-      type: 'book',
-      contributor: 'Kurt Vonnegut',
-      showDetail: false,
-    },
-    {
-      title: 'Better Call Saul',
-      description: "A slow-burning Breaking Bad prequel.",
-      type: 'streaming video',
-      contributor: '',
-      showDetail: false,
-    }
-  ]
-
-
-
-
-
-const app = new Vue({
-    el: '#media-list',
+const token = '7562922180.1677ed0.fc4e273eea1049be86e4a335cb642dbc';
+const insUrl = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + token;
+ new Vue({
+    el: '#app',
     data () {
       return {
-        title: 'Treehouse Public Library',
-        mediaList: media,
-        type: '',
-        info: null
+        media: [],
+        acc: {
+          title: "Hello"
+        }
       }
     },
     mounted () {
-       fetch('https://dog.ceo/api/breeds/image/random')
-
-   .then(
-     function(response) {
-       if (response.status !== 200) {
-         console.log('Looks like there was a problem. Status Code: ' +
-           response.status);
-         return;
-       }
-
-       // Examine the text in the response
-       response.json().then(function(data) {
-         console.log(data);
-       });
-     }
-   )
-   .catch(function(err) {
-     console.log('Fetch Error :-S', err);
-   });
+        axios.get(insUrl)
+        .then(response => {
+      // JSON responses are automatically parsed.
+      this.media = response.data.data
+      })
     },
     methods: {
-
       toggleDetails: function(media) {
         console.log(media.title);
         media.showDetail = !media.showDetail
